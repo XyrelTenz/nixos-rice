3a597bb2e2193e8bf17ac0d49a3fd00454c7caab
XyrelTenz <xdemocrito1@gmail.com>
2026-06-30 15:25:46 +0800

    nixos: remove non-existent prisma engine environment variables
    the global nixos environment variables configured for the query
    engine and prisma formatter pointed to paths that do not exist under
    the newer nixpkgs prisma-engines derivation, which caused prisma client
    resolution to crash. removing them allows prisma to fallback to its default
    Wasm or nix-ld wrapped query engine binaries.
    
    changelog: Fixed Prisma client query engine resolution crash on NixOS

--- modules/environment.nix
+++ modules/environment.nix
@@ -17,9 +17,6 @@
     QML_IMPORT_PATH  = "/run/current-system/sw/lib/qt-6/qml";
 
     PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
-    PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
-    PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
-    PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
   };
 
   programs.fish.enable = true;
